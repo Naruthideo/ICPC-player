8abf619763f9421d7805ce8cf99d077e2de06fcf
Naruthideo <cyrille.dejemeppe@gmail.com>
2011-01-24 22:10:29 +0100

    Releasing the player comparison script in version 1.0

--- scripts/player_comparison.py
+++ scripts/player_comparison.py
@@ -2,25 +2,25 @@
 #
 # ICPC challenge 2011 - Dejemeppe Cyrille & Mouthuy Francois-Xavier
 
-import sys, getopt, commands, math
+import sys, getopt, commands, math, time, os
 
 def one_play(player_1, player_2):
-    out_play = commands.getoutput('java -jar ../icypc.jar -player pipe 2 python ' + player_1 + ' -player pipe 2 ' + player_2 + ' -view trace tmp.txt')
-    last_line = out_play[-1].split()
+    out_play = commands.getoutput('java -jar ../icypc.jar -player pipe 2 python ' + player_1 + ' -player pipe 2 python ' + player_2 + ' -view trace tmp.txt')
+    last_line = out_play.splitlines()[-1].split()
     p1_score = int(last_line[1])
     p2_score = int(last_line[4])
     p_hat = float(p1_score - p2_score)/(p1_score + p2_score)
     return p_hat
 
-def k_fold(nb_plays, player_1, player_2, verbose=False):
+def k_fold(nb_plays, player_1, player_2, beg_time, verbose=False):
     p_bar = 0.0
     square_sum = 0.0
     p_hats = list()
     # Getting the results of the nb_plays plays
     for i in xrange(nb_plays):
         if verbose:
-            print 'Playing play #'+str(i+1)
-        p_hats.append(one_play())
+            print 'Playing play '+str(i+1)+' of '+str(nb_plays)+' (Total time elapsed: '+str(round(time.time()- beg_time,3))+' seconds)'
+        p_hats.append(one_play(player_1, player_2))
         p_bar += p_hats[-1]
     p_bar /= nb_plays
     for p_hat in p_hats:
@@ -59,23 +59,21 @@ def main():
             except:
                 default_usage()
     
+    beg_time = time.time()
     t_025_inf = 1.96
-    T = k_fold(nb_plays, args[0], args[1], verbose)
+    T = k_fold(nb_plays, args[0], args[1], beg_time, verbose)
+    os.remove('tmp.txt')
+    print '\n=====>>>>> Results computed in ' + str(round(time.time()-beg_time,3)) + ' seconds <<<<<=====\n'
     print '95% confidence interval:\n\tRR = [-inf; -1.96] U [1.96; inf]'
-    print 'Test statistic:\n\tT =', T
+    print 'Test statistic:\n\tT = ' + str(round(T,3))
     if T < -1.96:
-        print 'We accept, with 95% confidence, the hypothesis that the true performances are significantly different.'
+        print '\nWe accept, with 95% confidence, the hypothesis that the true performances are significantly different.'
         print 'And the best player is:\n\t\t', args[1]
     elif T > 1.96:
-        print 'We accept, with 95% confidence, the hypothesis that the true performances are significantly different.'
+        print '\nWe accept, with 95% confidence, the hypothesis that the true performances are significantly different.'
         print 'And the best player is:\n\t\t', args[0]
     else:
-        print 'We reject, with 95% confidence, the hypothesis that the true performances are significantly different.'
-        print 'These two players are thus equivalent in terms of performances.'
-        
-
-    
-    print '--verbose:', verbose
-    print '--nb_plays', nb_plays
-
+        print '\nWe reject, with 95% confidence, the hypothesis that the true performances are significantly different.'
+        print '\tThese two players are thus equivalent in terms of performances.'
+    print '\n==========>>>>>>>>>> The end <<<<<<<<<<==========\n'
 main()
